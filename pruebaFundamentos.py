a0f3eb22dd60eaa42139b0b0d1826b25fc6acf88
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-01-24 12:09:50 -0500

    alto voltaje

--- pruebaFundamentos.py
+++ pruebaFundamentos.py
@@ -1,1 +1,12 @@
 #Codigo fuente 45 voltajes
+cont = 5
+con = 0
+vol = []
+prom = con/5
+for i in range(cont):
+    vol[i] = int(input("Ingrese un valor de voltaje"))
+    con = con + vol[i]
+    if prom > 220:
+        print("Alto voltaje")
+    else:
+        print("Voltaje correcto")
