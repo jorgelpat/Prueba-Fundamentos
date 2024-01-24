8ce05aed49e53a34d9a3f86e744ddc46598816d8
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-01-24 12:32:42 -0500

    voltaje 2

---
+++ voltaje2.py
@@ -0,0 +1,14 @@
+cont = 3
+con = 0
+for i in range(cont):
+    vol = int(input("Ingrese un valor de voltaje:"))
+    con = con + vol
+prom = con/3
+
+if prom <= 115:
+    print("Voltaje Correcto")
+elif prom > 115 and prom < 220:
+    print("Alto Voltaje")
+else:
+    print("PELIGRO")
+
